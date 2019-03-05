74136f780f9dd99f8f9e35d814917a6517ca7c44
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-03-05 19:19:04 +0300

    Add core tables and filling of chessmen

---
+++ Chess/Filling.sql
@@ -0,0 +1,67 @@
+USE Chess;
+GO
+
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'knight', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'knight', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'bishop', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'bishop', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'rook  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'rook  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'queen ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'king  ', N'white')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'pawn  ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'knight', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'knight', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'bishop', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'bishop', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'rook  ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'rook  ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'queen ', N'black')
+GO
+INSERT ChessCore.Chessmen (ChessType, ChessColor) VALUES (N'king  ', N'black')
+GO
---
+++ Chess/Tables.sql
@@ -0,0 +1,31 @@
+USE Chess;
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = 'ChessCore')
+BEGIN
+EXEC('CREATE SCHEMA ChessCore')
+END
+
+IF OBJECT_ID('ChessCore.Chessboard', 'U') IS NOT NULL
+DROP TABLE ChessCore.Chessboard
+GO
+
+IF OBJECT_ID('ChessCore.Chessmen', 'U') IS NOT NULL
+DROP TABLE ChessCore.Chessmen
+GO
+
+CREATE TABLE ChessCore.Chessmen
+(
+    Id INT NOT NULL PRIMARY KEY IDENTITY,
+    ChessType NVARCHAR(40) NOT NULL CHECK(ChessType IN ('king', 'queen', 'rook', 'bishop', 'knight', 'pawn')),
+    ChessColor NVARCHAR(40) NOT NULL CHECK(ChessColor IN ('black', 'white'))
+);
+GO
+
+CREATE TABLE ChessCore.Chessboard
+(
+    ChessmanId INT NOT NULL PRIMARY KEY REFERENCES ChessCore.Chessmen(Id),
+    X NCHAR NOT NULL CHECK(X >= 'a' AND X <= 'h'),
+    Y NCHAR NOT NULL CHECK(Y >= '1' AND Y <= '8')
+);
+GO
