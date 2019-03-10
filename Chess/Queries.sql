c4d0f4efd166ac3675a39a0c689785301296e1dd
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-03-10 23:45:18 +0300

    Add tasks 1, 2, 4 + task list in pdf

--- Chess/Queries.sql
+++ Chess/Queries.sql
@@ -1,8 +1,27 @@
-USE Chess;
+USE Chess
 GO
 
 SELECT * FROM ChessCore.Chessmen
 GO
 
 SELECT * FROM ChessCore.Chessboard
+GO
+
+-- 1)
+SELECT COUNT(*) AS ChessCount FROM ChessCore.Chessboard
+GO
+
+-- 2)
+SELECT Id FROM ChessCore.Chessmen
+WHERE ChessType LIKE 'k%'
+GO
+
+-- 3)
+
+
+-- 4)
+SELECT ChessmanId FROM ChessCore.Chessboard, ChessCore.Chessmen
+WHERE ChessCore.Chessboard.ChessmanId = ChessCore.Chessmen.Id
+AND ChessColor = 'white'
+AND ChessType = 'pawn'
 GO
