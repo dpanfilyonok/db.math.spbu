10edcb14a08e0ccac0398413f55dd81633cc4e76
Dmitriy <dmitriy.panfilyonok@gmail.com>
2019-03-24 00:04:48 +0300

    Add task 5 and 11

--- Chess/Queries.sql
+++ Chess/Queries.sql
@@ -7,6 +7,15 @@ GO
 SELECT * FROM ChessCore.Chessboard
 GO
 
+CREATE VIEW ChessSummary AS 
+SELECT * FROM ChessCore.Chessboard INNER JOIN ChessCore.Chessmen
+ON ChessCore.Chessboard.ChessmanId = ChessCore.Chessmen.Id
+GO
+
+SELECT ASCII('a');
+SELECT ASCII('1');
+GO
+
 -- 1)
 SELECT COUNT(*) AS ChessCount FROM ChessCore.Chessboard
 GO
@@ -23,45 +32,58 @@ ORDER BY TypeCount DESC
 GO
 
 -- 4)
-SELECT ChessmanId FROM ChessCore.Chessboard INNER JOIN ChessCore.Chessmen
-ON ChessCore.Chessboard.ChessmanId = ChessCore.Chessmen.Id
-AND ChessColor = 'white'
-AND ChessType = 'pawn'
+SELECT ChessmanId FROM ChessSummary
+WHERE ChessColor = 'white' AND ChessType = 'pawn'
 GO
 
 -- 5)
-
+SELECT ChessType, ChessColor FROM ChessSummary
+WHERE ASCII(X) - ASCII(Y) = ASCII('a') - ASCII('1')
 
 -- 6)
-SELECT ChessColor, Count(*) AS FigureAmount FROM ChessCore.Chessboard INNER JOIN ChessCore.Chessmen
-ON ChessCore.Chessboard.ChessmanId = ChessCore.Chessmen.Id
+SELECT ChessColor, Count(*) AS FigureAmount FROM ChessSummary
 GROUP BY ChessColor
 GO
 
 -- 7)
-SELECT DISTINCT ChessType FROM ChessCore.Chessboard INNER JOIN ChessCore.Chessmen
-ON ChessCore.Chessboard.ChessmanId = ChessCore.Chessmen.Id
-AND ChessColor = 'black'
+SELECT DISTINCT ChessType FROM ChessSummary
+WHERE ChessColor = 'black'
 GO
 
 -- 8)
-SELECT DISTINCT ChessType, Count(*) AS FigureAmount FROM ChessCore.Chessboard INNER JOIN ChessCore.Chessmen
-ON ChessCore.Chessboard.ChessmanId = ChessCore.Chessmen.Id
-AND ChessColor = 'black'
+SELECT DISTINCT ChessType, Count(*) AS FigureAmount FROM ChessSummary
+WHERE ChessColor = 'black'
 GROUP BY ChessType
 GO
 
 -- 9)
-SELECT DISTINCT ChessType FROM ChessCore.Chessboard INNER JOIN ChessCore.Chessmen
-ON ChessCore.Chessboard.ChessmanId = ChessCore.Chessmen.Id
+SELECT DISTINCT ChessType FROM ChessSummary
 GROUP BY ChessType
 HAVING COUNT(*) >= 2
 
 -- 10)
-SELECT TOP 1 WITH TIES ChessColor  FROM ChessCore.Chessboard INNER JOIN ChessCore.Chessmen
-ON ChessCore.Chessboard.ChessmanId = ChessCore.Chessmen.Id
+SELECT TOP 1 WITH TIES ChessColor FROM ChessSummary
 GROUP BY ChessColor
 ORDER BY COUNT(*)
 GO
 
--- 12)
+-- 11)
+CREATE FUNCTION GetFigureCoordinates(@chessType NVARCHAR(40), @chessColor NVARCHAR(40))
+RETURNS TABLE AS
+RETURN (
+    SELECT X, Y FROM ChessSummary
+    WHERE ChessType = @chessType AND ChessColor = @chessColor
+)
+GO
+
+CREATE FUNCTION GetFigureOnRookWay(@x NCHAR, @y NCHAR)
+RETURNS TABLE AS 
+RETURN (
+    SELECT ChessType, ChessColor FROM ChessSummary
+    WHERE X = @x OR Y = @y
+)
+GO
+
+SELECT DISTINCT ChessType, ChessColor FROM dbo.GetFigureCoordinates('rook', 'white') coord
+CROSS APPLY dbo.GetFigureOnRookWay(coord.X, coord.Y)
+GO
